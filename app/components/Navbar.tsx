fb23d314e7e0be1be8a3330f27f158fb7982c4d9
NOCKTOWL <mehedi.hasanhulknabil@gmail.com>
2025-04-30 08:02:40 +0600

    feat: enhance Navbar component layout and styling; update global styles

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -11,25 +11,50 @@ import Link from 'next/link'
 
 export default function Navbar() {
   return (
-    <div>
+    <div className='flex justify-between items-center h-[84px] w-full top-0 px-4 py-10'>
+        {/* LOGO */}
+
         <Link href="/" className="flex items-center justify-center gap-2">
             <Image src={Logo} alt="Logo" className="w-[25px] h-[34px]" />
         </Link>
+
+        {/* DATE AND SEARCH FIELD */}
+
         <div className='flex justify-between gap-2'>
-            <input type="date" className='w-[260px] border-[1px] border-tertiary-light-gray px-2 pr-2 pl-4' />
+            <input type="date" className='w-[260px] border-[1px] border-tertiary-light-gray px-2 pr-2 pl-4 outline-none' />
              
-            <div className='relative flex justify-between'>
-                <input type="text" placeholder='Search' />
-                <Image src={Search} alt="Search" className='absolute top-1/2 left-2 -translate-y-1/2' />
+            <div className='relative flex justify-between w-65 h-13 pl-4 pr-2 py-2 border-1 border-tertiary-light-gray rounded-[99px]'>
+                <input type="text" placeholder='Search' className='placeholder:text-[#6A6A6A] text-sm outline-none'/>
+                <div className='absolute top-1/2 right-2 -translate-y-1/2 bg-deep-light-gray rounded-full p-[7px] cursor-pointer hover:brightness-95 transition-all duration-300 ease-in-out'>
+                    <Image src={Search} alt="Search" className='flex' />
+                </div>
             </div>
         </div>
-        <div className='flex gap-2 '>
-            <div className='flex justify-between items-center h-[50px] bg-light-yellow px-[14px] pl-[12px] pr-[16px] rounded-[99px]'>
-                <Image src={Leaderboard} alt="Leaderboard" className='w-[13px] h-[18px]' />
+
+        {/* LEADERBOARD, LOGIN/SIGNUP AND PROFILE */}
+
+        <div className='flex gap-2 text-secondary-pure-green'>
+            <div className='flex gap-[5px] justify-between items-center h-[50px] bg-light-yellow py-[14px] pl-3 pr-4 rounded-[99px]'>
+                
+                <div className='size-5 flex justify-center items-center'>
+                    <Image src={Leaderboard} alt="Leaderboard" width={13} height={18} />
+                </div>
+
                 <h1 className='text-base font-medium'>Leaderboard</h1>
             </div>
-            <div></div>
-            <div></div>
+
+            <div className='px-6 py-[14px] rounded-[99px] border-[1px] border-secondary-pure-green flex justify-center items-center'>
+                <h1 className='text-secondary-pure-green text-base font-medium leading-5'>Login / Sign up</h1>
+            </div>
+
+            <div className='flex gap-[10px] justify-between items-center h-[50px] pl-[10px] pr-2 py-[6px] border-1 rounded-[99px] border-deep-light-gray'>
+                <div className='size-[22px] flex justify-center items-center'>
+                    <Image src={Hamburger} alt="Hamburger" className='w-[13px] h-[9px]' />
+                </div>
+                <div className='size-[37px] flex justify-center items-center'>
+                    <Image src={Profile} alt="Profile" className='size-[30px]' />
+                </div>
+            </div>
         </div>
     </div>
   )
